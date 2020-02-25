c6aa9bbfd39430a435f9684a99e9eecef300b42e
Dennis <d.koukal@web.de>
2020-02-25 15:45:08 +0100

    Added a function for tracking unitids and their names

---
+++ modules/synergy-tracker/gunits.lua
@@ -0,0 +1,23 @@
+local ARS = ARS
+
+ARS.gunit = {}
+
+local function OnEffectChanged(_, _, _, _, _, _, _, _, _, _, _, _, _, unitName, unitId)
+	unitName = zo_strformat("<<1>>", unitName)
+	if unitName ~= "Offline" then
+		if ARS.gunit[unitId] ~= unitName then
+			ARS.gunit[unitId] = unitName
+			d("Registering " .. unitName .. " as " .. unitId)
+		else
+			ARS.gunit[unitId] = unitName
+		end
+	end
+end
+
+function ARS.GetNameForUnitId(unitId)
+	return ARS.gunit[unitId] or ""
+end
+
+function ARS.RegisterUnitIndexing()
+	EVENT_MANAGER:RegisterForEvent(ARS.name .. "UpdateUnitList", EVENT_EFFECT_CHANGED, OnEffectChanged)
+end
