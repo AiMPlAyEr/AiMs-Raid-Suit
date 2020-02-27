874227b46c90a062757efe695b89e8473d75c38e
AiMPlAyEr <46943296+AiMPlAyEr@users.noreply.github.com>
2020-02-27 18:20:14 +0100

    Fixed some synergy tracking bugs

--- modules/synergy-tracker/core.lua
+++ modules/synergy-tracker/core.lua
@@ -98,13 +98,12 @@ local function UpdateGroup()
 end
 
 local function GetSynergy(eventCode, result, _, abilityName, _, _, _, sourceType, _, targetType, _, _, _, _, sourceUnitId, targetUnitId, abilityId)
-    if result ~= ACTION_RESULT_EFFECT_GAINED then return end
 
     local getunit = ARS.GetNameForUnitId(targetUnitId)
+    d(getunit)
 
     --adding unit to synergypool
     for k, v in ipairs(synergypool) do
-        --checking if user exists in table
         if v.name == getunit then
             if getunit ~= "" and ARS.Synergies[abilityId] == 1 then
                 synergypool[k].primarysynergy = GetGameTimeSeconds() + 20
@@ -125,53 +124,59 @@ function UpdateTimer()
 
     if gsize == 0 then return end
 
-    for i = 1, gsize + 1 then
-        local accName   = GetUnitDisplayName("group" .. i)
+    for i = 1, gsize + 1 do
+        local accName   = GetRawUnitName("group" .. i)
+        accName = zo_strformat("<<1>>", accName)
         local role      = GetGroupMemberAssignedRole("group" .. i)
 
-        if accName ~= nil and role ~= 0 then
+        if accName ~= nil then
             synergypool[i]                  = {}
             synergypool[i].name             = accName
+            synergypool[i].online           = role
             synergypool[i].primarysynergy   = "0"
             synergypool[i].secondarysynergy = "0"
         end
     end
-
 end
+
+function roundNum(num, numDecimalPlaces)
+    local mult = 10^(numDecimalPlaces or 0)
+    return math.floor(num * mult + 0.5) / mult
+  end
 
 function UpdateCooldown()
     for k, v in ipairs(synergypool) do
-        local pRemainingTime = v.primarysynergy - GetGameTimeSeconds()
-        local sRemainingTime = v.secondarysynergy - GetGameTimeSeconds()
+        local pRemainingTime = roundNum(v.primarysynergy - GetGameTimeSeconds())
+        local sRemainingTime = roundNum(v.secondarysynergy - GetGameTimeSeconds())
 
         groupunit = pool:AcquireObject(k)
 
-        if pRemainingTime > "5" then
-            groupunit.primarysynergy:SetColor(220, 20, 60)
+        if pRemainingTime > 5 then
+            groupunit.primarysynergy:SetColor(255, 0, 0)
             groupunit.primarysynergy:SetText(pRemainingTime)
         end
 
-        if pRemainingTime < "6" and pRemainingTime > "0" then
+        if pRemainingTime < 6 and pRemainingTime > 0 then
             groupunit.primarysynergy:SetColor(255, 255, 0)
             groupunit.primarysynergy:SetText(pRemainingTime)
         end
 
-        if pRemainingTime == "0" then
+        if pRemainingTime == 0 then
             groupunit.primarysynergy:SetColor(255, 255, 255)
             groupunit.primarysynergy:SetText("0")
         end
 
-        if sRemainingTime > "5" then
+        if sRemainingTime > 5 then
             groupunit.secondarysynergy:SetColor(220, 20, 60)
             groupunit.secondarysynergy:SetText(sRemainingTime)
         end
 
-        if sRemainingTime < "6" and sRemainingTime > "0" then
+        if sRemainingTime < 6 and sRemainingTime > 0 then
             groupunit.secondarysynergy:SetColor(255, 255, 0)
             groupunit.secondarysynergy:SetText(sRemainingTime)
         end
 
-        if sRemainingTime == "0" then
+        if sRemainingTime == 0 then
             groupunit.secondarysynergy:SetColor(255, 255, 255)
             groupunit.secondarysynergy:SetText("0")
         end
@@ -188,14 +193,17 @@ function ARS:InitializeSynergyTracker(enable)
 
     ARS.RegisterUnitIndexing()
 
-    UpdateGroup()
     UpdateTimer()
+    UpdateGroup()
 
     EVENT_MANAGER:RegisterForEvent(ARS.name.."GroupUpdate",EVENT_GROUP_MEMBER_JOINED , UpdateGroup)
     EVENT_MANAGER:RegisterForEvent(ARS.name.."GroupUpdate",EVENT_GROUP_MEMBER_LEFT , UpdateGroup)
     EVENT_MANAGER:RegisterForEvent(ARS.name.."GroupUpdate",EVENT_GROUP_MEMBER_ROLE_CHANGED , UpdateGroup)
     EVENT_MANAGER:RegisterForEvent(ARS.name.."GroupUpdate",EVENT_GROUP_MEMBER_CONNECTED_STATUS , UpdateGroup)
-    EVENT_MANAGER:RegisterForEvent(ARS.name.."Synergy", EVENT_COMBAT_EVENT, GetSynergy)
+    for k, v in pairs(ARS.Synergies) do
+        EVENT_MANAGER:RegisterForEvent(ARS.name.."Synergy"..k, EVENT_COMBAT_EVENT, GetSynergy)
+        EVENT_MANAGER:AddFilterForEvent(ARS.name.."Synergy"..k, EVENT_COMBAT_EVENT, REGISTER_FILTER_ABILITY_ID, k)
+    end
     EVENT_MANAGER:RegisterForUpdate(ARS.name.."UpdateCooldown", 100, UpdateCooldown)
     --EVENT_MANAGER:RegisterForEvent("MyAddon", EVENT_COMBAT_EVENT, UpdateBuffs)
 end
